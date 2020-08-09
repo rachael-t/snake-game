b96dd5162200394a53528ff0f142a914af0618d2
Rachael Thomas <rnthomas001@gmail.com>
2020-08-09 16:10:54 -0600

    Add query selectors and pseudo code our createGrid function

---
+++ index.js
@@ -0,0 +1,10 @@
+const grid = document.querySelector(".grid");
+const startButton = document.getElementById("start");
+const score = document.getElementById("score");
+
+function createGrid() {
+    // create elements that will be nested within grid
+    // create 100 of these elements 
+    // add styling to these elements
+    // create array of squares
+}
