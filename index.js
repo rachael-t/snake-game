2c39e867a92fd2cd899bbbc2772c4ce80f4d83b1
Rachael Thomas <rnthomas001@gmail.com>
2020-08-09 17:24:48 -0600

    Create apple function and coloring to randomly populate a square that is not occupied by the snake

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
-const grid = document.querySelector('.grid');
-const startButton = document.getElementById('start');
-const score = document.getElementById('score');
+const grid = document.querySelector(".grid");
+const startButton = document.getElementById("start");
+const score = document.getElementById("score");
 let squares = [];
-let currentSnake = [2,1,0];
+let currentSnake = [2, 1, 0];
 let direction = 1;
 const width = 10;
+let appleIndex = 0;
 
 function createGrid() {
-    for (let i=0; i < width*width; i++) {
-        const square = document.createElement('div')
-        square.classList.add('square')
-        grid.appendChild(square)
-        squares.push(square)
-    }
-};
+  for (let i = 0; i < width * width; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    grid.appendChild(square);
+    squares.push(square);
+  }
+}
 
 createGrid();
 
-currentSnake.forEach(index => squares[index].classList.add('snake'));
+currentSnake.forEach((index) => squares[index].classList.add("snake"));
 
 function move() {
-    if ( 
-        (currentSnake[0] + width >= width*width && direction === width) ||
-        (currentSnake[0] % width === width-1 && direction === 1) ||
-        (currentSnake[0] % width === 0 && direction === -1) ||
-        (currentSnake[0] - width < 0 && direction === -width) || 
-        squares[currentSnake[0] + direction].classList.contains('snake')
-        ) {
-        return clearInterval(timerId)
-    }
+  if (
+    (currentSnake[0] + width >= width * width && direction === width) ||
+    (currentSnake[0] % width === width - 1 && direction === 1) ||
+    (currentSnake[0] % width === 0 && direction === -1) ||
+    (currentSnake[0] - width < 0 && direction === -width) ||
+    squares[currentSnake[0] + direction].classList.contains("snake")
+  ) {
+    return clearInterval(timerId);
+  }
 
-    const tail = currentSnake.pop();
-    squares[tail].classList.remove('snake');
-    currentSnake.unshift(currentSnake[0] + direction);
-    squares[currentSnake[0]].classList.add('snake');
+  const tail = currentSnake.pop();
+  squares[tail].classList.remove("snake");
+  currentSnake.unshift(currentSnake[0] + direction);
+  squares[currentSnake[0]].classList.add("snake");
 };
 
 move();
 
 let timerId = setInterval(move, 1000);
 
+function generateApples() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake'))
+    squares[appleIndex].classList.add('apple')
+};
+
+generateApples();
+
 function control(e) {
-    if (e.keyCode === 39) {
-        direction = 1
-    } else if (e.keyCode === 38) {
-        direction = -width
-    } else if (e.keyCode === 37) {
-        direction = -1
-    } else if (e.keyCode === 40) {
-        direction = +width
-    }
+  if (e.keyCode === 39) {
+    direction = 1;
+  } else if (e.keyCode === 38) {
+    direction = -width;
+  } else if (e.keyCode === 37) {
+    direction = -1;
+  } else if (e.keyCode === 40) {
+    direction = +width;
+  }
 }
 
-document.addEventListener('keyup', control)
+document.addEventListener("keyup", control);
